1e976f0d742754392763b8da08c11fc3e468d29c
martingarnica902 <martingarnica05@gmail.com>
2026-09-21 15:53:38 -0300

    Convert ReservaRepository to interface with JPA methods
    Cambie las clases repository restantes con referencia a los cambios de elliot

--- Backend/sysgepolidep/src/main/java/tecleros/sysgepolidep/repository/ReservaRepository.java
+++ Backend/sysgepolidep/src/main/java/tecleros/sysgepolidep/repository/ReservaRepository.java
@@ -1,4 +1,17 @@
 package tecleros.sysgepolidep.repository;
 
-public class ReservaRepository {
+import tecleros.sysgepolidep.entity.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+
+    // Listar reservas asociadas a un socio
+    List<Reserva> findBySocioId(Long socioId);
+
+    // Listar reservas para una instalación en particular
+    List<Reserva> findByInstalacionId(Long instalacionId);
 }
