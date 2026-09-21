5fc706aa2911893325ca47c5dbd2713d07ff8aed
martingarnica902 <martingarnica05@gmail.com>
2026-09-21 16:16:20 -0300

    Update PagoService.java
    Realice los cambios en las clases services y repository en base a las modificaciones de referencia de elliot

--- Backend/sysgepolidep/src/main/java/tecleros/sysgepolidep/service/PagoService.java
+++ Backend/sysgepolidep/src/main/java/tecleros/sysgepolidep/service/PagoService.java
@@ -1,4 +1,32 @@
 package tecleros.sysgepolidep.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tecleros.sysgepolidep.entity.Pago;
+import tecleros.sysgepolidep.repository.PagoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PagoService {
+
+    @Autowired
+    private PagoRepository pagoRepository;
+
+    public List<Pago> obtenerTodos() {
+        return pagoRepository.findAll();
+    }
+
+    public Optional<Pago> obtenerPorId(Long id) {
+        return pagoRepository.findById(id);
+    }
+
+    public Pago guardar(Pago pago) {
+        return pagoRepository.save(pago);
+    }
+
+    public void eliminar(Long id) {
+        pagoRepository.deleteById(id);
+    }
 }
