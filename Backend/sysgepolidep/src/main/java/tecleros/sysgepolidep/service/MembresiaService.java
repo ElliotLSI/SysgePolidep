8818aa459476f50c9475d642e4a8f5f2fe32fc5d
martingarnica902 <martingarnica05@gmail.com>
2026-09-21 16:17:44 -0300

    Implement MembresiaService with CRUD operations
    Realice los cambios en las clases services y repository en base a las modificaciones de referencia de elliot

--- Backend/sysgepolidep/src/main/java/tecleros/sysgepolidep/service/MembresiaService.java
+++ Backend/sysgepolidep/src/main/java/tecleros/sysgepolidep/service/MembresiaService.java
@@ -1,4 +1,32 @@
 package tecleros.sysgepolidep.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tecleros.sysgepolidep.entity.Membresia;
+import tecleros.sysgepolidep.repository.MembresiaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MembresiaService {
+
+    @Autowired
+    private MembresiaRepository membresiaRepository;
+
+    public List<Membresia> obtenerTodas() {
+        return membresiaRepository.findAll();
+    }
+
+    public Optional<Membresia> obtenerPorId(Long id) {
+        return membresiaRepository.findById(id);
+    }
+
+    public Membresia guardar(Membresia membresia) {
+        return membresiaRepository.save(membresia);
+    }
+
+    public void eliminar(Long id) {
+        membresiaRepository.deleteById(id);
+    }
 }
