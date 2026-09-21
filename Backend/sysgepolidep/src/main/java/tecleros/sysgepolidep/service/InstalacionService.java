278e33af07a0921545c4bdfffb423e765d54d4b9
martingarnica902 <martingarnica05@gmail.com>
2026-09-21 16:18:40 -0300

    Implement InstalacionService methods for CRUD operations
    Realice los cambios en las clases services y repository en base a las modificaciones de referencia de elliot.

--- Backend/sysgepolidep/src/main/java/tecleros/sysgepolidep/service/InstalacionService.java
+++ Backend/sysgepolidep/src/main/java/tecleros/sysgepolidep/service/InstalacionService.java
@@ -1,4 +1,32 @@
 package tecleros.sysgepolidep.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tecleros.sysgepolidep.entity.Instalacion;
+import tecleros.sysgepolidep.repository.InstalacionRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class InstalacionService {
+
+    @Autowired
+    private InstalacionRepository instalacionRepository;
+
+    public List<Instalacion> obtenerTodas() {
+        return instalacionRepository.findAll();
+    }
+
+    public Optional<Instalacion> obtenerPorId(Long id) {
+        return instalacionRepository.findById(id);
+    }
+
+    public Instalacion guardar(Instalacion instalacion) {
+        return instalacionRepository.save(instalacion);
+    }
+
+    public void eliminar(Long id) {
+        instalacionRepository.deleteById(id);
+    }
 }
