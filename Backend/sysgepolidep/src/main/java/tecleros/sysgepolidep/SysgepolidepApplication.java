abd61789badb773595ba9a874e1c783186a3ec79
ElliotLSI <tumail@ejemplo.com>
2026-09-21 11:50:42 -0300

    feat: estructura inicial limpia del sistema

---
+++ Backend/sysgepolidep/src/main/java/tecleros/sysgepolidep/SysgepolidepApplication.java
@@ -0,0 +1,13 @@
+package tecleros.sysgepolidep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysgepolidepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysgepolidepApplication.class, args);
+	}
+
+}
