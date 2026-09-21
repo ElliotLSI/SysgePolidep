8f53d9c7a430ff5a149fd42e79ea5a2a8cccf911
ElliotLSI <tumail@ejemplo.com>
2026-09-21 14:12:25 -0300

    Complete lo correspondiente a esas clases. usar de guia para trabajar

--- Backend/sysgepolidep/src/main/java/tecleros/sysgepolidep/repository/UsuarioRepository.java
+++ Backend/sysgepolidep/src/main/java/tecleros/sysgepolidep/repository/UsuarioRepository.java
@@ -1,4 +1,16 @@
 package tecleros.sysgepolidep.repository;
 
-public class UsuarioRepository {
+import tecleros.sysgepolidep.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    // Buscar por DNI (útil para validar si ya existe antes de registrar)
+    Optional<Usuario> findByDni(String dni);
+
+    // Buscar por Nombre de Usuario
+    Optional<Usuario> findByNombreUsuario(String nombreUsuario);
 }
