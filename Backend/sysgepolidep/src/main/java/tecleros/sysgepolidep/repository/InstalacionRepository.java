a740d3782a60b05a38f090442c34d300b8c27ef7
martingarnica902 <martingarnica05@gmail.com>
2026-09-21 16:25:47 -0300

    Refactor InstalacionRepository to use JpaRepository
    Realice los cambios en las clases services y repository en base a las modificaciones de referencia de elliot

--- Backend/sysgepolidep/src/main/java/tecleros/sysgepolidep/repository/InstalacionRepository.java
+++ Backend/sysgepolidep/src/main/java/tecleros/sysgepolidep/repository/InstalacionRepository.java
@@ -1,4 +1,17 @@
 package tecleros.sysgepolidep.repository;
 
-public class InstalacionRepository {
+import tecleros.sysgepolidep.entity.Instalacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InstalacionRepository extends JpaRepository<Instalacion, Long> {
+
+    // Listar instalaciones pertenecientes a una categoría
+    List<Instalacion> findByCategoriaId(Long categoriaId);
+
+    // Listar instalaciones disponibles
+    List<Instalacion> findByDisponible(Boolean disponible);
 }
