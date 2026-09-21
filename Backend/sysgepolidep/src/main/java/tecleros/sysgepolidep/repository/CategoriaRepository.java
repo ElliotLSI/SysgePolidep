5d97ec4aa96bd307b05c4ebbe22a79fc167f76eb
martingarnica902 <martingarnica05@gmail.com>
2026-09-21 16:26:53 -0300

    Refactor CategoriaRepository to use JpaRepository
    Realice los cambios en las clases services y repository en base a las modificaciones de referencia de elliot

--- Backend/sysgepolidep/src/main/java/tecleros/sysgepolidep/repository/CategoriaRepository.java
+++ Backend/sysgepolidep/src/main/java/tecleros/sysgepolidep/repository/CategoriaRepository.java
@@ -1,4 +1,14 @@
 package tecleros.sysgepolidep.repository;
 
-public class CategoriaRepository {
+import tecleros.sysgepolidep.entity.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    // Buscar categoría por su nombre
+    Optional<Categoria> findByNombre(String nombre);
 }
