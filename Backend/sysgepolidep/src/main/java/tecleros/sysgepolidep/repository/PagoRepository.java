89a02851b1b9e1f065658f96febe83ad51811d98
martingarnica902 <martingarnica05@gmail.com>
2026-09-21 16:21:48 -0300

    Implement PagoRepository interface for payment management
    Realice los cambios en las clases services y repository en base a las modificaciones de referencia de elliot

--- Backend/sysgepolidep/src/main/java/tecleros/sysgepolidep/repository/PagoRepository.java
+++ Backend/sysgepolidep/src/main/java/tecleros/sysgepolidep/repository/PagoRepository.java
@@ -1,4 +1,14 @@
 package tecleros.sysgepolidep.repository;
 
-public class PagoRepository {
+import tecleros.sysgepolidep.entity.Pago;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PagoRepository extends JpaRepository<Pago, Long> {
+
+    // Listar historial de pagos de un socio
+    List<Pago> findBySocioId(Long socioId);
 }
